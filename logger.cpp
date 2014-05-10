5c468ca1b33ed4393a25c4c82b3bb7f62d5a88a2
Mike Axford <m1ari@m1ari.co.uk>
2014-05-10 13:00:59 +0100

    File Logging Class

---
+++ logger.cpp
@@ -0,0 +1,107 @@
+#include <string>
+#include <ctime>
+#include <sys/time.h>
+#include <cstdio>
+#include "logger.h"
+
+
+namespace UKHASnet {
+	Logger::Logger(){
+		path="log";
+		logfd=NULL;
+		logtime=0;
+	}
+	Logger::Logger(std::string p, std::string n){
+		setPath(p);
+		setName(n);
+		logfd=NULL;
+		logtime=0;
+	}
+	Logger::~Logger(){
+		if (logfd!=NULL){
+			struct timeval now;
+			if ( gettimeofday(&now,NULL) <0 ){
+				perror("Logger");
+				return;
+			}
+			struct tm tm_now;
+			gmtime_r(&now.tv_sec,&tm_now);
+			closeLog(&tm_now);
+		}
+	}
+
+	void Logger::setPath(std::string p){
+		path=p;
+	}
+	void Logger::setName(std::string n){
+		name=n;
+	}
+
+	void Logger::openLog(struct tm *tm_now){
+		if (logfd == NULL){
+			char buff[100];
+			snprintf(buff,100,"%s/%s-%04d-%02d-%02d.log",
+				path.c_str(), name.c_str(),
+				(tm_now->tm_year+1900), (tm_now->tm_mon+1), tm_now->tm_mday
+			);
+
+			logfd=fopen(buff, "a");
+			if (logfd==NULL){
+				perror("Logger");
+				return;
+			}
+			strftime(buff,100,"<%F>-<%T>",tm_now);
+			fprintf(logfd, "===== Opening Log %s =====\n",buff);
+		}
+	}
+
+	void Logger::closeLog(struct tm *tm_now){
+		if (logfd != NULL){
+			char buff[100];
+			strftime(buff,100,"<%F>-<%T>",tm_now);
+			fprintf(logfd, "===== Closing Log %s =====\n",buff);
+			fclose(logfd);
+			logfd=NULL;     // Do we need to do this ??
+		}
+	}
+
+	void Logger::writeLog(std::string type, std::string msg){
+		struct timeval now;
+		if ( gettimeofday(&now,NULL) <0 ){
+			perror("Logger");
+			return;
+		}
+
+		struct tm tm_now, tm_log;
+		gmtime_r(&now.tv_sec,&tm_now);
+		gmtime_r(&logtime,&tm_log);
+
+		if ((tm_now.tm_year != tm_log.tm_year) || (tm_now.tm_yday != tm_log.tm_yday) ){
+			closeLog(&tm_now);
+			openLog(&tm_now);
+			logtime=now.tv_sec;
+		}
+
+		// Trim any \r\n from the end of string - Other options at
+		// http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+		msg.erase(msg.find_last_not_of("\n\r")+1);
+
+		if (logfd!=NULL){
+			if (type.empty()){
+				fprintf(logfd, "[%02d:%02d:%02d.%03ld] %s\n", 
+					tm_now.tm_hour, tm_now.tm_min, tm_now.tm_sec, (now.tv_usec/1000),
+					msg.c_str()
+				);
+			} else {
+				fprintf(logfd, "[%02d:%02d:%02d.%03ld] %s: %s\n", 
+					tm_now.tm_hour, tm_now.tm_min, tm_now.tm_sec, (now.tv_usec/1000),
+					type.c_str(), msg.c_str()
+				);
+			}
+			fflush(logfd);
+		} else {
+			// TODO rate limit error messages
+			fprintf(stderr, "Error(Logger): Not writing logs to file\n");
+		}
+	}
+}
---
+++ logger.h
@@ -0,0 +1,33 @@
+#ifndef logger_h_
+#define logger_h_
+
+/* Maintains the file handles for a log file and logrotate (occurs for the
+ * first message logged passed midnight)
+ */
+
+namespace UKHASnet {
+
+class Logger {
+	private:
+		FILE *logfd;
+		time_t logtime;
+		std::string path;
+		std::string name;
+
+		void openLog(struct tm *tm_now);
+		void closeLog(struct tm *tm_now);
+
+	public:
+		Logger();
+		Logger(std::string path, std::string name);
+		~Logger();
+		void setPath(std::string path);
+		void setName(std::string name);
+		void setFile(std::string file);
+		void writeLog(std::string type, std::string msg);
+
+	protected:
+};
+
+}
+#endif
